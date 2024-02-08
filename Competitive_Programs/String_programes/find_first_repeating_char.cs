0927463c89343ca8740aed2c00965b8eea71a20d
sushant jadhav <sushantjadhav416@gmail.com>
2024-02-08 12:58:52 +0530

    new code

---
+++ Competitive_Programs/String_programes/find_first_repeating_char.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.String_programes
+{
+    internal class find_first_repeating_char
+    {
+        String firstRepChar(String s)
+        {
+            // code here
+            bool[] seen = new bool[26];
+            foreach(char c in s.ToCharArray())
+            {
+                if (seen[c - 'a'])
+                {
+                    return "" + c;
+                }
+                seen[c - 'a'] = true;
+            }
+            return "-1";
+        }
+    }
+}
