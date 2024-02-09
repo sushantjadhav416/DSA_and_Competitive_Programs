5f66b21ea9ed44dd2cb096dad5590a4672fad9ba
sushant jadhav <sushantjadhav416@gmail.com>
2024-02-09 15:00:56 +0530

    initial

---
+++ Competitive_Programs/Searching_algos/Find_a_pair_with_the_given_difference.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Searching_algos
+{
+    internal class Find_a_pair_with_the_given_difference
+    {
+
+        static bool findPair(int[] arr, int n)
+        {
+            int size = arr.Length;
+
+            // Initialize positions of two elements
+            int i = 0, j = 1;
+
+            // Search for a pair
+            while (i < size && j < size)
+            {
+                if (i != j && (arr[j] - arr[i] == n || arr[i] - arr[j] == n))
+                {
+                    Console.WriteLine("Pair Found: " +
+                                     "( " + arr[i] + ", " + arr[j] + " )");
+                    return true;
+                }
+                else if (arr[j] - arr[i] < n)
+                    j++;
+                else
+                    i++;
+            }
+
+            return false;
+        }
+
+        
+    }
+}
---
+++ Competitive_Programs/Searching_algos/Searching_in_an_array_where_adjacent_differ_by_at_most_k.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Searching_algos
+{
+    internal class Searching_in_an_array_where_adjacent_differ_by_at_most_k
+    {
+        static int search(int[] arr, int n,
+                           int x, int k)
+        {
+
+            // Traverse the given array starting
+            // from leftmost element
+            int i = 0;
+
+            while (i < n)
+            {
+
+                // If x is found at index i
+                if (arr[i] == x)
+                    return i;
+
+                // Jump the difference between 
+                // current array element and x
+                // divided by k We use max here
+                // to make sure that i moves 
+                // at-least one step ahead.
+                i = i + Math.Max(1, Math.Abs(arr[i]
+                                          - x) / k);
+            }
+
+
+            return -1;
+        }
+    }
+}
