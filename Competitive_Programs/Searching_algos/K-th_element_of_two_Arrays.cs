26812373ae3e896a00efca041657e7f98eed22da
sushant jadhav <sushantjadhav416@gmail.com>
2024-02-11 19:11:57 +0530

    new codes

---
+++ Competitive_Programs/Searching_algos/K-th_element_of_two_Arrays.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Searching_algos
+{
+    internal class K_th_element_of_two_Arrays
+    {
+        public long kthElement(int[] arr1, int[] arr2, int n, int m, int k_req)
+        {
+            int i = 0, j = 0, k = 0;
+            while (i < n && j < m)
+            {
+                if (arr1[i] < arr2[j])
+                {
+                    k++;
+                    if (k == k_req)
+                        return (long)arr1[i];
+                    i++;
+                }
+                else
+                {
+                    k++;
+                    if (k == k_req)
+                        return (long)arr2[j];
+                    j++;
+                }
+            }
+
+            while (i < n)
+            {
+                k++;
+                if (k == k_req)
+                    return (long)arr1[i];
+                i++;
+            }
+
+            while (j < m)
+            {
+                k++;
+                if (k == k_req)
+                    return (long)arr2[j];
+                j++;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Competitive_Programs/Searching_algos/Single_Element_in_a_Sorted_Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Searching_algos
+{
+    internal class Single_Element_in_a_Sorted_Array
+    {
+        public int singleNonDuplicate(int[] nums)
+        {
+
+
+            int l = 0;
+            int r = nums.Length - 1;
+
+            while (l < r)
+            {
+                int mid = (l + r) / 2;
+
+                if (mid % 2 == 1)
+                    --mid;
+                if (nums[mid] == nums[mid + 1])
+                    l = mid + 2;
+                else
+                    r = mid;
+            }
+            return nums[l];
+        }
+    }
+}
