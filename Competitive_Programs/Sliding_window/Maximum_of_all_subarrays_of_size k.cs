6d0b046da06b5a9eae439f6d5b7b27b4def90faf
sushant jadhav <sushantjadhav416@gmail.com>
2024-02-20 22:53:41 +0530

    new codes

---
+++ Competitive_Programs/Sliding_window/Maximum_of_all_subarrays_of_size k.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Sliding_window
+{
+    internal class Maximum_of_all_subarrays_of_size_k
+    {
+        static List<int> max_of_subarrays(int[] arr, int n, int k)
+        {
+            List<int> Al = new List<int>();
+            int j, max = 0;
+            for (int i = 0; i <= n - k; i++)
+            {
+                max = arr[i];
+                for (j = 1; j < k; j++)
+                {
+                    if (max < arr[i + j])
+                        max = arr[i + j];
+                }
+                Al.Add(max);
+            }
+
+            return Al;
+        }
+    }
+}
