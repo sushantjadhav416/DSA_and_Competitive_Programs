fa303f4a78180b4f27c91eee9e6714f57adddd9d
sushant jadhav <sushantjadhav416@gmail.com>
2024-02-22 12:30:55 +0530

    new codes

---
+++ Competitive_Programs/Numbers_and_bit_manupaltion/Bitwise_AND_of_Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Numbers_and_bit_manupaltion
+{
+    internal class Bitwise_AND_of_Numbers
+    {
+       public int rangeBitwiseAnd(int m, int n)
+        {
+            int shiftBits = 0;
+
+            while (m != n)
+            {
+                m >>= 1;
+                n >>= 1;
+                ++shiftBits;
+            }
+
+            return m << shiftBits;
+        }
+    }
+}
---
+++ Competitive_Programs/String_programes/String_to_number_Atio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.String_programes
+{
+    internal class String_to_number_Atio
+    {
+        public int myAtoi(String s)
+        {
+            
+            if (s.Length==0)
+                return 0;
+
+            int sign = s[0] == '-' ? -1 : 1;
+            if (s[0] == '+' || s[0] == '-')
+                s = s.Substring(1);
+
+            long num = 0;
+
+            foreach( char c in  s.ToCharArray())
+            {
+                if (!char.IsDigit(c))
+                    break;
+                num = num * 10 + (c - '0');
+                if (sign * num <= int.MinValue)
+                    return int.MinValue;
+                if (sign * num >= int.MaxValue)
+                    return int.MaxValue;
+            }
+
+            return sign * (int)num;
+        }
+    }
+}
