e17665658ff687b409f4d3e02da513733c926887
sushant jadhav <sushantjadhav416@gmail.com>
2024-02-27 23:34:46 +0530

    changes

--- Competitive_Programs/Searching_algos/K-th_element_of_two_Arrays.cs
+++ Competitive_Programs/Searching_algos/K-th_element_of_two_Arrays.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace Competitive_Programs_and_DSA_Qns.Searching_algos
 {
     internal class K_th_element_of_two_Arrays
     {
+
+       // Input : Array 1 - 2 3 6 7 9
+       // Array 2 - 1 4 8 10
+       // k = 5
+       //Output : 6
+       //Explanation: The final sorted array would be -
+       //1, 2, 3, 4, 6, 7, 8, 9, 10
+       //The 5th element of this array is 6.
+
         public long kthElement(int[] arr1, int[] arr2, int n, int m, int k_req)
         {
             int i = 0, j = 0, k = 0;
