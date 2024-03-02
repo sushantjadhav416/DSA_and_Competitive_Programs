f3bb1c92bbe25308c8910e6580dc57dce53739c5
sushant jadhav <sushantjadhav416@gmail.com>
2024-03-02 23:53:56 +0530

    Todays codes

---
+++ Competitive_Programs/Dynamic_programing/Trapping_Rain_Water.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Dynamic_programing
+{
+    internal class Trapping_Rain_Water
+    {
+        static long trappingWater(int[] arr, int n)
+        {
+            // Your code here
+            long[] left= new long[n];
+
+            long[] right = new long[n];
+
+            long water = 0;
+
+            left[0] = arr[0];
+            for (int i = 1; i < n; i++)
+                left[i] = Math.Max(left[i - 1], arr[i]);
+
+            right[n - 1] = arr[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+                right[i] = Math.Max(right[i + 1], arr[i]);
+
+
+            for (int i = 0; i < n; i++)
+                water += Math.Min(left[i], right[i]) - arr[i];
+
+            return water;
+
+        }
+    }
+}
---
+++ Competitive_Programs/Heap_DS/Does_array_represent_Heap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Heap_DS
+{
+    internal class Does_array_represent_Heap
+    {
+        public bool countSub(long[] arr, long n)
+        {
+            // Your code goes here
+            for (int i = 0; i <= (n - 2) / 2; i++)
+            {
+                if (arr[2 * i + 1] > arr[i])
+                {
+                    return false;
+                }
+
+                if (2 * i + 2 < n && arr[2 * i + 2] > arr[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
