21918656047c7ff078b21a404caf3b06f1bf73a4
sushant jadhav <sushantjadhav416@gmail.com>
2024-03-04 23:37:56 +0530

    new codes

---
+++ Competitive_Programs/Heap_DS/Heap_Height.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Heap_DS
+{
+    internal class Heap_Height
+    {
+        static int height(int N)
+        {
+            return (int) Math.Ceiling(Math.Log(N + 1) / Math.Log(2)) - 1;
+        }
+    }
+}
