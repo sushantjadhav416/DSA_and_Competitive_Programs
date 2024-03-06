cb9c34ac03f19c38c6a77529607c2acc822a2cd4
sushant jadhav <sushantjadhav416@gmail.com>
2024-03-06 15:12:29 +0530

    new codes

---
+++ Competitive_Programs/Arrays/Type_of_array.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Arrays
+{
+    internal class pair
+    {
+        long first, second;
+        public pair(long first, long second)
+        {
+            this.first = first; 
+            this.second = second;
+        }
+
+    }
+
+    internal class Type_of_array
+    {
+        static pair maxNtype(long[] arr, long n)
+        {
+
+            long max = 0;
+            long min = 0;
+
+            long type = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (max < arr[i])
+                    max = arr[i];
+
+                if (min > arr[i])
+                    min = arr[i];
+            }
+
+            if (arr[0] == min && arr[arr.Length - 1] == max)
+                type = 1;
+            else if (arr[0] == max && arr[arr.Length - 1] == min)
+                type = 2;
+            else if (arr[0] < arr[arr.Length - 1])
+                type = 3;
+            else
+                type = 4;
+
+
+            return new pair(type, max);
+        }
+    }
+}
---
+++ Competitive_Programs/Heap_DS/Is_Binary_Tree_Heap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Heap_DS
+{
+    class Node
+    {
+       internal  int data;
+       internal  Node left, right;
+        Node(int d)
+        {
+            data = d;
+            left = right = null;
+        }
+    }
+    internal class Is_Binary_Tree_Heap
+    {
+        int countNodes(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            return 1 + countNodes(root.left) + countNodes(root.right);
+
+        }
+
+        bool completeBh(Node root, int index, int countNode)
+        {
+            if (root == null)
+                return true;
+
+            if (index >= countNode)
+                return false;
+
+            return completeBh(root.left, 2 * index + 1, countNode) && completeBh(root.right, 2 * index + 2, countNode);
+        }
+
+        bool IsBHeap(Node tree)
+        {
+            if (tree.left == null && tree.right == null)
+                return true;
+
+            if (tree.right == null)
+            {
+                return tree.data >= tree.left.data;
+            }
+            else
+            {
+                if (tree.data >= tree.left.data && tree.data >= tree.right.data)
+                    return IsBHeap(tree.left) && IsBHeap(tree.right);
+                else
+                    return false;
+            }
+
+        }
+
+
+        bool isHeap(Node tree)
+        {
+            // code here
+            if (tree == null)
+                return true;
+
+            if ((completeBh(tree, 0, countNodes(tree)) == true) && (IsBHeap(tree) == true))
+                return true;
+
+
+            return false;
+        }
+    }
+}
