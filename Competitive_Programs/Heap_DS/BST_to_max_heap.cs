bc321d5173691e499029a2f0f0fa6ccfe4afd704
sushant jadhav <sushantjadhav416@gmail.com>
2024-03-07 18:05:14 +0530

    new codes added

---
+++ Competitive_Programs/Heap_DS/BST_to_max_heap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Heap_DS
+{
+    internal class BST_to_max_heap
+    {
+        static int i;
+        public static void inorder(Node root, List<int> arr)
+        {
+            if (root == null)
+                return;
+
+            inorder(root.left, arr);
+            arr.Add(root.data);
+            inorder(root.right, arr);
+        }
+
+        public static void BSTToMaxHeap(Node root, List<int> arr)
+        {
+            if (root == null)
+                return;
+
+            BSTToMaxHeap(root.left, arr);
+
+            BSTToMaxHeap(root.right, arr);
+
+            root.data = arr.ElementAt(i++);
+
+        }
+
+        public static void convertToMaxHeapUtil(Node root)
+        {
+            //code here
+            if (root == null)
+                return;
+
+
+            List<int> arr = new List<int>();
+            int i = -1;
+
+            inorder(root, arr);
+
+            BSTToMaxHeap(root, arr);
+
+        }
+    }
+}
---
+++ Competitive_Programs/Heap_DS/Convert_Min_Heap_to_Max_Heap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Heap_DS
+{
+    internal class Convert_Min_Heap_to_Max_Heap
+    {
+        static void Heapify(int[] arr, int i, int N)
+        {
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            int largest = i;
+
+            if (l < N && arr[l] > arr[i])
+                largest = l;
+            if (r < N && arr[r] > arr[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = temp;
+
+                Heapify(arr, largest, N);
+            }
+
+        }
+
+        static void convertMinToMaxHeap(int N, int[] arr)
+        {
+            // code here
+            for (int i = (N - 2) / 2; i >= 0; --i)
+                Heapify(arr, i, N);
+        }
+    }
+}
