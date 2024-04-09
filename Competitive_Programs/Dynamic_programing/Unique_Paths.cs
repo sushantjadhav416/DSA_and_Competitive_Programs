4b73a24275d4a1dace05a3399bae6fd32937bf74
sushant jadhav <sushantjadhav416@gmail.com>
2024-04-09 23:45:16 +0530

    new codes added in Bit-manuplation , DP, and recursion

---
+++ Competitive_Programs/Dynamic_programing/Unique_Paths.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Dynamic_programing
+{
+    internal class Unique_Paths
+    {
+        public int uniquePaths(int m, int n)
+        {
+            int[,] dp = new int[m,n];
+
+            for (int i = 1; i < m; ++i)
+                for (int j = 1; j < n; ++j)
+                    dp[i,j] = dp[i - 1,j] + dp[i,j - 1];
+
+            return dp[m - 1,n - 1];
+        }
+    }
+}
---
+++ Competitive_Programs/Recursion/Number_of_Steps-to_Reduce_a_Number_to_Zero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Recursion
+{
+    internal class Number_of_Steps_to_Reduce_a_Number_to_Zero
+    {
+        public int numberOfSteps(int num)
+        {
+            if (num == 0)
+                return 0;
+
+            int steps = 0;
+            while (true)
+            {
+
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    steps += 1;
+                }
+                else
+                {
+                    num = num - 1;
+                    steps += 1;
+                    if (num == 0)
+                        return steps;
+                    num /= 2;
+                    steps += 1;
+                }
+
+                if (num == 0)
+                    return steps;
+            }
+
+
+
+        }
+    }
+}
