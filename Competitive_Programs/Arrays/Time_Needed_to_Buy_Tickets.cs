b1c12b327e6b3d44d482476b73409aa7e788d1ef
sushant jadhav <sushantjadhav416@gmail.com>
2024-04-11 13:20:56 +0530

    New codes added in Arrays and Binary tree

---
+++ Competitive_Programs/Arrays/Time_Needed_to_Buy_Tickets.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Arrays
+{
+     /// PS:
+    /// There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line. 
+    /// You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].  Each person takes exactly 1 second to buy a ticket.A person can only buy 1 ticket at a time and has to go back to the end of the line(which happens instantaneously) in order to buy more tickets.If a person does not have any tickets left to buy, the person will leave the line.
+     /// Return the time taken for the person at position k (0-indexed) to finish buying tickets.
+    
+    internal class Time_Needed_to_Buy_Tickets
+    {
+        public int timeRequiredToBuy(int[] tickets, int k)
+        {
+
+            int ans = 0;
+
+            for (int i = 0; i < tickets.Length; i++)
+            {
+                if (i <= k)
+                {
+                    ans += Math.Min(tickets[i], tickets[k]);
+                }
+                else
+                {
+                    ans += Math.Min(tickets[i], tickets[k] - 1);
+                }
+            }
+
+            return ans;
+
+        }
+    }
+}
---
+++ Competitive_Programs/Binary_tree/Maximum_Width_of_Tree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Binary_tree
+{
+    internal class Maximum_Width_of_Tree
+    {
+        //Calculating Width at each level
+        int getWidth(Node root, int level)
+        {
+            if (root == null)
+                return 0;
+
+            if (level == 1)
+            {
+                return 1;
+            }
+            if (level > 1)
+            {
+                return getWidth(root.left, level - 1) + getWidth(root.right, level - 1);
+            }
+
+            return 0;
+        }
+
+
+        //calculate Total hight of a tree
+        int getHight(Node root)
+        {
+            if (root == null)
+                return 0;
+            else
+            {
+
+                int L = getHight(root.left);
+                int R = getHight(root.right);
+
+
+                return (L > R) ? L + 1 : R + 1;
+
+            }
+        }
+
+        //calculate maximum width by calculating width at each level of hight
+        int getMaxWidth(Node root)
+        {
+            // Your code here
+
+            int maxWidth = 0;
+
+            int width = 0;
+
+            int h = getHight(root);
+
+            for (int i = 0; i <= h; i++)
+            {
+                width = getWidth(root, i);
+                if (width > maxWidth)
+                {
+                    maxWidth = width;
+                }
+            }
+
+            return maxWidth;
+
+        }
+    }
+}
