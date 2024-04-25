05e09f4370acd838d044a852701afcf478a69674
sushant jadhav <sushantjadhav416@gmail.com>
2024-04-25 18:41:14 +0530

    new code added in linked list

---
+++ Competitive_Programs/Linked_List/Delete_the_Middle_Node-of_a-Linked_List.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Linked_List
+{
+    internal class Delete_the_Middle_Node_of_a_Linked_List
+    {
+        public int getLength(Node head)
+        {
+            int len = 0;
+            Node MyN = head;
+            while (MyN != null)
+            {
+                len++;
+                MyN = MyN.next;
+            }
+            return len;
+        }
+
+        public Node deleteMiddle(Node head)
+        {
+            int length = getLength(head);
+
+            if (length == 1)
+                return null;
+
+            Node curr = head;
+            for (int i = 0; i < ((length / 2) - 1); i++)
+            {
+                curr = curr.next;
+            }
+
+            curr.next = curr.next.next;
+
+            return head;
+
+        }
+    }
+}
