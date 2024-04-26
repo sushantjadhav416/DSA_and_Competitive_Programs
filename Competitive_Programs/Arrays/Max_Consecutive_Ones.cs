55a50601aa08b2a58a7fe5c56b234d65d6309981
sushant jadhav <sushantjadhav416@gmail.com>
2024-04-26 22:57:18 +0530

    new codes added in Array and Matrix

---
+++ Competitive_Programs/Arrays/Max_Consecutive_Ones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Arrays
+{
+    internal class Max_Consecutive_Ones
+    {
+        public int findMaxConsecutiveOnes(int[] nums)
+        {
+            int c = 0;
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    c++;
+                }
+                else
+                {
+                    c = 0;
+                }
+
+                max = Math.Max(max, c);
+            }
+
+            return max;
+
+        }
+    }
+}
