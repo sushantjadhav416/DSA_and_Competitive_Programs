b330f7cfc4d4de1a6bc45efb655d7ab876ee69f9
Sushant jadhav <sushantjadhav416@gmail.com>
2024-05-11 20:39:26 +0530

    problem added in heap(priority_queue)

---
+++ Competitive_Programs/Heap_DS/Kth_Largest_Element_in_an_Array.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programs_and_DSA_Qns.Heap_DS
+{
+    internal class Kth_Largest_Element_in_an_Array
+    {
+       public int findKthLargest(int[] nums, int k) 
+    {
+    //   Queue<Integer> minHeap = new PriorityQueue<Integer>();
+
+    //   for(int num:nums)
+    //   {
+    //       minHeap.offer(num);
+
+    //       while(minHeap.size()>k)
+    //       {
+    //           minHeap.poll();
+    //       }
+    //   } 
+
+    //    return minHeap.peek(); 
+    return 0;
+    }
+    }
+}
