bb1fab1dc57e375c70fadd0e0d17a3975ac8618a
Sushant jadhav <sushantjadhav416@gmail.com>
2024-05-11 19:15:26 +0530

    problem added in Arrays

--- Competitive_Programs/Arrays/Merge_sorted_array.cs
+++ Competitive_Programs/Arrays/Merge_sorted_array.cs
@@ -57,6 +57,25 @@ namespace Competitive_Programs.Arrays
 
         }
 
+        public void merge(int[] nums1, int m, int[] nums2, int n) 
+       {
+        int i = m - 1;     // nums1's index (the actual nums)
+        int j = n - 1;     // nums2's index
+        int k = m + n - 1; // nums1's index (the next filled position)
+
+        while (j >= 0)
+        {
+           if (i >= 0 && nums1[i] > nums2[j])
+           {
+                 nums1[k--] = nums1[i--];
+           }
+           else
+           {
+                  nums1[k--] = nums2[j--];
+           }
+        }
+       }
+
         public static void s_Main(string[] args)
         {
             int[] arr1 = { 1, 7, 9, 3, 4, 0, 0, 0 };
